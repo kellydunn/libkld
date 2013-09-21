b925cfd07fa909599498299babe2fc1de3ac929f
kellydunn <defaultstring@gmail.com>
2013-09-20 18:30:55 -0700

    [src] Simple matrix implementation

---
+++ src/matrix/matrix.c
@@ -0,0 +1,28 @@
+#include "matrix.h"
+
+kld_matrix_t * new_matrix() {
+  kld_matrix_t * m = calloc(1, sizeof(kld_matrix_t *));
+  m->data = NULL;
+  return m;
+}
+
+kld_vector_t * matrix_get_row(kld_matrix_t * m, int y) {
+  return m->data[y];
+}
+
+kld_vector_t * matrix_get_col(kld_matrix_t * m, int x) {
+  kld_vector_t * res = (kld_vector_t *) new_vector();
+  
+  // TODO Oof, O(N) for getting a column?  
+  //      Lets research to see if we can do better.
+  int i;
+  for(i = 0; i < m->y_bounds; i++) {
+    vector_insert(res, vector_get(matrix_get_row(m, i), x));
+  }
+
+  return res;
+}
+
+void * matrix_get(kld_matrix_t * m, int x, int y) {
+  return vector_get(matrix_get_row(m, y), x);
+}
--- src/matrix/matrix.h
+++ src/matrix/matrix.h
@@ -7,11 +7,13 @@ typedef struct kld_matrix kld_matrix_t;
 
 struct kld_matrix {
   kld_vector_t ** data;
+  int x_bounds;
+  int y_bounds;
 };
 
 kld_matrix_t * new_matrix();
-kld_vector_t * matrix_get_col(int x);
-kld_vector_t * matrix_get_row(int y);
-void * matrix_get(int x, int y);
+kld_vector_t * matrix_get_col(kld_matrix_t * m, int x);
+kld_vector_t * matrix_get_row(kld_matrix_t * m, int y);
+void * matrix_get(kld_matrix_t * m, int x, int y);
 
 #endif
