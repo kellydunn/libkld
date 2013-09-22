5561a2e8d0088707ed07faf65b07a7b5c3008ad3
kellydunn <defaultstring@gmail.com>
2013-09-22 16:33:36 -0700

    [src][test] Adding in matrix is empty test.  Expanding matrix init test.

--- src/matrix/matrix.h
+++ src/matrix/matrix.h
@@ -8,19 +8,22 @@ typedef struct kld_matrix kld_matrix_t;
 /*
  * A Somewhat Non Standard Implementation of a matrix
  *
- * The current design of this matrix is to use two collections of <kld_vector_t>, one to represent all the rows inside of the <kld_matrix_t> and another to represent all the colums.
+ * The current design of this matrix is to use two collections of <kld_vector_t>, 
+ * one to represent all the rows inside of the <kld_matrix_t> and another to represent all the colums.
  * This effectively doubles the memory footprint of a <kld_matrix_t>, requiring 2 bytes for each entry.
- * However, due to the underlying implementation of a <kld_vector_t>, we can now garuntee constant time operations for retrieving single elements inside of the <kld_matrix_t> as well as entire rows and columns.
+ * However, due to the underlying implementation of a <kld_vector_t>, 
+ * we can now garuntee constant time operations for retrieving single elements inside of the <kld_matrix_t> 
+ * as well as entire rows and columns.
+ * It should be noted that adding operations have an ammortized constant speed, but if the underlying <kld_vector_t>
+ * need to grow, they will have to reallocate memory accordingly.  Inserting rows or colums in such a scenario could
+ * cause adding operations to be O(rc), where **r** is the number of rows and **c** is the number of columns.
  */
 struct kld_matrix {
   kld_vector_t ** rows;
   kld_vector_t ** cols;
   int x_bounds;
   int y_bounds;
-  int capacity;
 };
-
-extern const int DEFAULT_MATRIX_CAPACITY;
 
 kld_matrix_t * new_matrix();
 bool matrix_is_empty();
--- tests/matrix/matrix_test.c
+++ tests/matrix/matrix_test.c
@@ -6,13 +6,22 @@ START_TEST (test_new_matrix) {
   kld_matrix_t * m = (kld_matrix_t*) new_matrix();
   fail_if(m->rows != NULL, "Unexpected rows after initialization");
   fail_if(m->cols != NULL, "Unexpected cols after initialization");
-} END_TEST;
+  fail_if(m->x_bounds != 0, "Unexpected x bounds after initialization");
+  fail_if(m->y_bounds != 0, "Unexpected y bounds after initialization");
+} END_TEST
+
+START_TEST(test_matrix_is_empty) {
+  kld_matrix_t * m = (kld_matrix_t *) new_matrix();
+  fail_if(!matrix_is_empty(m), "Matrix should be empty after initialization.");
+} END_TEST
 
 Suite * new_matrix_suite() {
   Suite * s = suite_create("matrix");
   
   TCase * tc_core = tcase_create("core");
   tcase_add_test(tc_core, test_new_matrix);
+
+  tcase_add_test(tc_core, test_matrix_is_empty);
 
   suite_add_tcase(s, tc_core);
   
