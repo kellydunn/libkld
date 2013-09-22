2619169f53baa651d9fa241d765eabc44c7e3c04
kellydunn <defaultstring@gmail.com>
2013-09-22 03:00:35 -0700

    [src] Adding in some documentation for lists and vectors

--- src/list/list.h
+++ src/list/list.h
@@ -9,7 +9,7 @@ typedef struct kld_list_node kld_list_node_t;
 /* 
  * A Standard List implementation.
  *
- * <kld_list_t> is implemented as a doubly-linked circular linked list.
+ * <kld_list_t> is implemented as a circular, doubly-linked list.
  * It's flexible in that it can serve as a stack or as a queue in addition to a linked list by calling 
  * `list_append` then `list_pop` (for FILO operations), and `list_append` then `list_shift` (for FIFO operations) respectively.
  *
@@ -28,72 +28,71 @@ struct kld_list {
   kld_list_node_t * tail;
 };
 
+
+/*
+ * A Standard List Node implementation.
+ *
+ * <kld_list_node_t> is a supplementary data structure for <kld_list_t>. 
+ * It is a representation of a singular node of data within a <kld_list_t>.
+ *
+ */
 struct kld_list_node {
   void * data;
   kld_list_node_t * next;
   kld_list_node_t * prev;
 };
 
-/* 
- * Creates a new <kld_list_t>
+/* Creates a new <kld_list_t>
  * @return a pointer to a new <kld_list_t>
  */
 kld_list_t * new_list();
 
 
-/* 
- * Checks to see if a list is empty.
+/* Checks to see if a list is empty.
  * @list the list to be checked.
  * @return true if the list is empty, false otherwise.
  */
 bool list_is_empty(kld_list_t * list);
 
 
-/* 
- * Appends data to a list
+/* Appends data to a list
  * @list the list to append data to.
  * @data the data to append to the list.
  */
 void list_append(kld_list_t * list, void * data);
 
 
-/* 
- * Prepends data to a list
+/* Prepends data to a list
  * @list the list to prepend data to.
  * @data the data to prepend to the list.
  */
 void list_prepend(kld_list_t * list, void * data);
 
 
-/* 
- * Removes and returns the last <kld_list_node_t> in the <kld_list_t>.
+/* Removes and returns the last <kld_list_node_t> in the <kld_list_t>.
  * @list the list to pop the last element from.
  * @return <kld_list_node_t> in the passed in list.
  */
 kld_list_node_t * list_pop(kld_list_t * list);
 
 
-/* 
- * Returns a reversed copy of the passed in list.
+/* Returns a reversed copy of the passed in list.
  * @list the <kld_list_t> to be reversed.
  * @return a new <kld_list_t> that is the reversed value of the passed in one.
  */
 kld_list_t * list_reverse(kld_list_t * list);
 
 
-/* 
- * Removes and returns the first <kld_list_node_t> in the <kld_list_t>.
+/* Removes and returns the first <kld_list_node_t> in the <kld_list_t>.
  * @list the <kld_list_t> in which to remove the first <kld_list_node_t> from.
  * @return the first <kld_list_node_t> in the passed in <kld_list_t>
  */
 kld_list_node_t * list_shift(kld_list_t * list);
 
-void list_init_with_one(kld_list_t *list, kld_list_node_t * node);
 
-/*
- * Removes
- *
+/* Clears the <kld_list_t> and all its members
  */
-
 void list_clear(kld_list_t * list);
+
+
 #endif
--- src/vector/vector.h
+++ src/vector/vector.h
@@ -5,6 +5,12 @@
 
 typedef struct kld_vector kld_vector_t;
 
+/*
+ * A Standard implemetation of a Vector.
+ * 
+ * <kld_vector_t> is most similar to an array list, or a dynamic array.  It grows and shrinks according to the number of its elements.
+ * As to 
+ */
 struct kld_vector {
   void ** data;
   int size;
@@ -13,11 +19,44 @@ struct kld_vector {
 
 extern const int DEFAULT_VECTOR_CAPACITY;
 
+/* Creates and returns a pointer to a new <kld_vector_t>
+ * @return a pointer to a new <kld_vector_t>
+ */
 kld_vector_t * new_vector();
+
+
+/* Returns whether or not the vector is empty
+ * @v the vector in which to check emptiness.
+ * @return true if the vector has no elements or false if otherwise.
+ */
 bool vector_is_empty(kld_vector_t * v);
+
+
+/* Returns the data stored at the index **i** inside of <kld_vector_t> **v**.
+ * @v the <kld_vector_t> in which to get data.
+ * @i the index of the <kld_vector_t> in which to get data.
+ * @return the data stored at index **i** of <kld_vector_t> **v** passed in.
+ */
 void * vector_get(kld_vector_t * v, int i);
+
+/* Inserts the passed in **data** at index **i** in vector **v**.
+ * @v the <kld_vector_t> in which to insert the **data**.
+ * @i the index in which to insert into <kld_vector_t> **v**.
+ * @data the data in which to insert into <kld_vector_t> **v**.
+ */
 void vector_insert_at(kld_vector_t * v, int i, void * data);
+
+/* Appends the passed in **data** to vector **v**.
+ * @v the <kld_vector_t> in which to insert the **data**.
+ * @data the data in which to append into <kld_vector_t> **v**.
+ */
 void vector_append(kld_vector_t * v, void * data);
+
+/* Removes the passed in **data** at index **i** in vector **v**.
+ * @v the <kld_vector_t> in which to remove the **data**.
+ * @i the index in which to remove into <kld_vector_t> **v**.
+ * @data the data in which to remove into <kld_vector_t> **v**.
+ */
 void vector_remove_at(kld_vector_t * v, int i);
 
 #endif 
